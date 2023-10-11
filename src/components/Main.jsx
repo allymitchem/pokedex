11863410aa098432638a8d2016323286fd82d36f
Allyson Mitchem <allysonmitchem@gmail.com>
2023-10-11 16:52:40 -0600

    fixed issue with package.json adding onto url in localhost browser

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'   
+import React from 'react' 
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'  
 
 const Main = () => {
     return(
         <div id ="main">
-
+            <h2>Main</h2>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'React'
+import React from 'react'
 import ReactDom from 'react-dom/client'
+import {Main} from './components'
 
 
 
