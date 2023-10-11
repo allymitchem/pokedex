8fa145b5b1b6f53647b18bbd9b84dce43937a03c
Allyson Mitchem <allysonmitchem@gmail.com>
2023-10-11 16:07:23 -0600

    added boilerplate files/initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'React'
+import ReactDom from 'react-dom/client'
+
+
+
+const container = document.getElementById('app')
+const root = ReactDom.createRoot(container)
+
+root.render(<Main/>)
