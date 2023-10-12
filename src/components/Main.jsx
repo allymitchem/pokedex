cb90c2849c7ee8bee95cc3dde52d9a9e27b76f79
Allyson Mitchem <allysonmitchem@gmail.com>
2023-10-12 12:56:53 -0600

    trouble shooting background image/margins

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'  
 
 const Main = () => {
+
+    const backgroundImageStyle = {
+        backgroundImage: `url('https://res.cloudinary.com/fsa2/image/upload/v1697063592/portfolio%20site%20images/Project%20Images/pokemonBackground_xk0jbh.webp')`,
+        backgroundSize: 'cover',
+        // backgroundAttachment: 'fixed',
+        height: '110vh',
+        width: '100vw',
+        
+        
+        
+    }
     return(
-        <div id ="main">
-            <h2>Main</h2>
+        <div id ="main" style={backgroundImageStyle}>
+            {/* <h2>Main</h2> */}
         </div>
     )
 }
