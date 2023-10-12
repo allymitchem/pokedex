fe8ab7ce44d665e212d8bc728cc14c4d7c0c01b1
Allyson Mitchem <allysonmitchem@gmail.com>
2023-10-12 13:34:24 -0600

    added link for stylesheet that was missing in html for css not applying

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Title = () => {
     return(
-        <h2>title</h2>
+        <div className="title">
+            
+            <img id="titleImage" src="https://res.cloudinary.com/fsa2/image/upload/v1697137944/portfolio%20site%20images/Project%20Images/pokedex2_nhtjdp.png" alt="title"/>
+        </div>
     )
 }
 
