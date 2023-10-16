d3b79cdb5a96f878c88d628e6d9b513d106ac686
Allyson Mitchem <allysonmitchem@gmail.com>
2023-10-16 16:10:52 -0600

    added and tested getAllCards api function

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,26 @@
 const url = 'https://pokeapi.co/api/v2'
+export async function getAllCards() {
+    const reqObj = {
+        method:'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    try {
+        const response = await fetch(url+"/pokemon", reqObj)
+        const result = await response.json()
+        return result
+    } catch (error){
+        console.error(error)
+    }
+}
+const fetchData = async () => {
+    try {
+        const data = await getAllCards()
+        console.log(data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+fetchData()
