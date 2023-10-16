44696ace2fe06fda4b1c5994b0c9ad5ea547c034
Allyson Mitchem <allysonmitchem@gmail.com>
2023-10-16 15:43:57 -0600

    added test border for individual card, will need to style once API calls are made

--- src/components/IndividualCard.jsx
+++ src/components/IndividualCard.jsx
@@ -4,6 +4,15 @@ const IndividualCard = () => {
     return(
         <div className="individualCard">
             <h2>individual card</h2>
+            {/* NEEDS:
+                -Name
+                -Height?
+                -Weight
+                -HP
+                -Type(circular image)
+                -Image
+                -Moves
+             */}
         </div>
         
     )
