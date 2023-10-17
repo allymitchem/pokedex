ccab60933f02cd8b84b076b24e8610680f0e762c
Allyson Mitchem <allysonmitchem@gmail.com>
2023-10-17 12:28:32 -0600

    added function getByName

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express')
-// const fetch = require('node-fetch')
-const app = express()
-const port = 3000
+// const express = require('express')
 
-app.use(express.json())
+// const app = express()
+// const port = 3000
 
-app.get('/pokemon/:name', async (req, res) => {
-    const pokeApiUrl = `https://pokeapi.co/api/v2/pokemon/${req.params.name}`
-    try {
-        const response = await fetch(pokeApiUrl)
-        const data = await response.json()
-        res.json(data)
-    } catch (error){
-        console.error(error)
-        res.status(500).json({error: 'Failed to fetch data'})
-    }
-})
+// app.use(express.json())
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+// app.get('/pokemon/:name', async (req, res) => {
+//     const pokeApiUrl = `https://pokeapi.co/api/v2/pokemon/${req.params.name}`
+//     try {
+//         const response = await fetch(pokeApiUrl)
+//         const data = await response.json()
+//         res.json(data)
+//     } catch (error){
+//         console.error(error)
+//         res.status(500).json({error: 'Failed to fetch data'})
+//     }
+// })
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`)
+// })
