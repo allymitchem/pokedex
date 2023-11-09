128c0f932eed522dab95062dde76a933baf97678
Allyson Mitchem <allysonmitchem@gmail.com>
2023-11-09 14:25:57 -0700

    changed delay timer, added 'setLastSearchedPokemon' useState to useEffect

--- src/components/PokemonSearch.jsx
+++ src/components/PokemonSearch.jsx
@@ -1,24 +1,28 @@
 import React, {useEffect, useState} from "react";
 import {getByName} from "../api/index"
-import CardList from "./CardList";
-import IndividualCard from "./IndividualCard";
+// import CardList from "./CardList";
+// import IndividualCard from "./IndividualCard";
+
 
 
 const PokemonSearch = () => {
 
     const [searchTerm, setSearchTerm] = useState('')
     const [pokemon, setPokemon] = useState(null)
+    const [lastSearchedPokemon, setLastSearchedPokemon] = useState(null)
 
     useEffect(()=> {
-        const delay = 200
+        const delay = 500
         let timer
 
         const performSearch = async () => {
             try{
                 const data = await getByName(searchTerm)
+                setLastSearchedPokemon(data)
                 setPokemon(data)
             } catch (error){
                 console.error(error)
+                // setPokemon(null)
                 // setPokemon(null) --> clears previous data if error occurs
             }
         }
@@ -63,16 +67,17 @@ const PokemonSearch = () => {
             />
             {/* <button onClick={handleSearch}>Search</button> */}
             <div className="pokedexContainer">
-        {pokemon ? (
+        {searchTerm && pokemon && (
             <div className="individualCard">
                 <img className="pokemonImage" src ={pokemon.sprites.front_default} alt ={pokemon.name}/>
                 <h2 className="pokemonName">{pokemon.name}</h2>
                 <h3 className="pokemonId" >{pokemon.id}</h3>
                 
-            </div> ) : (
-                <p>no data available</p>
+            </div> 
+            // ) : (
+            //     <p >no data available</p>
             )}
-            </div>
+             </div>
             
         </div>
 
